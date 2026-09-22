ebc5579b41cf5ec84a1210bc66404a5c8cec1d82
eliandromadariaga-source <eliandromadariaga@gmail.com>
2026-09-21 23:12:31 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // Tu dirección web exacta en GitHub Pages
   site: 'https://eliandromadariaga-source.github.io',
-  
-  // El nombre de la carpeta/repositorio
-  base: 'felizc-umplea-osamor', 
+  base: '/felizc-umplea-osamor',
+  trailingSlash: 'always'
 });
